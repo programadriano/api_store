ed00de30382e1051caaecb516fdc23d6d3ba0613
Thiago <thiago.adriano26@gmail.com>
2018-08-24 05:44:43 -0300

    first commit

---
+++ api_store/Controllers/ValuesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_store.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_store.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        public List<Transactions> transactions;
+
+        public ValuesController()
+        {
+            transactions = new List<Transactions>
+            {
+                new Transactions
+                {
+                    Id = 1,
+                    Data = DateTime.Now,
+                    Loja = "Loja A",
+                    Produto = "Produto de teste",
+                    Total = 300,
+                    Vendido_para = "Comprador A"
+                },
+                 new Transactions
+                {
+                    Id = 2,
+                    Data = DateTime.Now,
+                    Loja = "Loja A",
+                    Produto = "Produto de teste",
+                    Total = 300,
+                    Vendido_para = "Comprador A"
+                },
+                  new Transactions
+                {
+                    Id = 3,
+                    Data = DateTime.Now,
+                    Loja = "Loja A",
+                    Produto = "Produto de teste",
+                    Total = 300,
+                    Vendido_para = "Comprador A"
+                },
+                   new Transactions
+                {
+                    Id = 4,
+                    Data = DateTime.Now,
+                    Loja = "Loja A",
+                    Produto = "Produto de teste",
+                    Total = 300,
+                    Vendido_para = "Comprador A"
+                },
+                    new Transactions
+                {
+                    Id = 5,
+                    Data = DateTime.Now,
+                    Loja = "Loja A",
+                    Produto = "Produto de teste",
+                    Total = 300,
+                    Vendido_para = "Comprador A"
+                }
+            };
+
+
+
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Transactions> Get()
+        {
+            return transactions.OrderBy(x => x.Data).ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IEnumerable<Transactions> Get(int id)
+        {
+            return transactions.Where(x => x.Id == id).OrderBy(x => x.Data).ToList();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Transactions transaction)
+        {
+            transactions.Add(transaction);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Transactions transaction)
+        {
+            var t = transactions.FirstOrDefault(x => x.Id == id);
+
+            if (t != null)
+            {
+                t.Data = transaction.Data;
+                t.Loja = transaction.Loja;
+                t.Produto = transaction.Produto;
+                t.Total = transaction.Total;
+                t.Vendido_para = transaction.Vendido_para;
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var t = transactions.FirstOrDefault(x => x.Id == id);
+
+            if (t != null)
+            {
+                transactions.Remove(t);
+            }
+        }
+    }
+}
---
+++ api_store/Model/Transactions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_store.Model
+{
+    public class Transactions
+    {
+
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public decimal Total { get; set; }
+        public string Produto { get; set; }
+        public string Loja { get; set; }
+        public string Vendido_para { get; set; }
+    }
+}
